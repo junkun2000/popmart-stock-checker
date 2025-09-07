8a026845f565249ec38cfafd970c4fc5c58791e3
junkun2000 <junjundayo0320@gmail.com>
2025-09-07 22:23:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
