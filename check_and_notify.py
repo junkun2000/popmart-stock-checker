7d8daf103a9c96a0a11e9e36af2a5f07fdf24328
junkun2000 <junjundayo0320@gmail.com>
2025-09-08 23:29:36 +0900

    Update check_and_notify.py

--- check_and_notify.py
+++ check_and_notify.py
@@ -5,11 +5,10 @@ from bs4 import BeautifulSoup
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # Google Sheets APIの認証情報
@@ -65,11 +64,13 @@ def check_stock():
     found_products = []
 
     # Seleniumの設定
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options.binary_location = "/usr/bin/google-chrome-stable"
+
+    driver = webdriver.Chrome(options=options)
 
     for url in URLS_TO_MONITOR:
         try:
